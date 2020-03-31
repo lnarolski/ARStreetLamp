c5fbd83a63fe98b78b60d35808b1bd83d7e04850
lnarolski <narolskilukasz+github@gmail.com>
2020-03-31 22:53:15 +0200

    Updated NuGet packages, can't start with internet examples

--- ARStreetLamp/MainActivity.cs
+++ ARStreetLamp/MainActivity.cs
@@ -3,6 +3,7 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Google.AR.Core;
 
 namespace ARStreetLamp
 {
@@ -15,6 +16,16 @@ namespace ARStreetLamp
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            var config = Google.AR.Core.();
+            session = new Session(this);
+
+            // Make sure ARCore is supported on this device
+            if (!session.IsSupported(config))
+            {
+                Toast.MakeText(this, "ARCore unsupported!", ToastLength.Long).Show();
+                Finish();
+            }
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
