1bff9716011cc7fbbe3ca196b88f7871625f0c76
lnarolski <narolskilukasz+github@gmail.com>
2020-06-16 22:49:35 +0200

    New lamp model

--- ARStreetLamp/obj/Debug/90/android/environment.arm64-v8a.s
+++ ARStreetLamp/obj/Debug/90/android/environment.arm64-v8a.s
@@ -70,7 +70,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"1d3e8382-0335-462f-b374-96db1af641fa"
+	.asciz	"39f57a11-67f3-44c1-a55f-240fb2fbb8ca"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
